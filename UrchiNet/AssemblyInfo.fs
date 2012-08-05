8f306ebd8f01faa07377bcacd841f3a961292dfb
mausch <mauricioscheffer@gmail.com>
2012-08-05 16:39:29 -0300

    version info

---
+++ UrchiNet/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace UrchiNet
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+[<assembly: AssemblyTitle("UrchiNet")>]
+[<assembly: AssemblyDescription("Urchin API v1 client")>]
+[<assembly: AssemblyProduct("UrchiNet")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2012")>]
+do()
