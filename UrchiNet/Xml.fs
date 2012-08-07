2589be5e0bffa71936ea4fc483f50a53e49f0c69
mausch <mauricioscheffer@gmail.com>
2012-08-07 16:44:20 -0300

    Remove unused functions

--- UrchiNet/Xml.fs
+++ UrchiNet/Xml.fs
@@ -20,30 +20,3 @@ module Xml =
         e.Attributes() 
         |> Seq.map (fun a -> a.Name.LocalName,a.Value)
         |> Seq.toList
-
-    /// <summary>
-    /// Matches a <see cref="XElement"/>
-    /// </summary>
-    /// <param name="n"></param>
-    let (|Tag|_|) (n: XNode) = 
-        match n with
-        | :? XElement as e -> Some e
-        | _ -> None
-
-    /// <summary>
-    /// Matches a <see cref="XElement"/>, splitting name, attributes and children
-    /// </summary>
-    /// <param name="n"></param>
-    let (|TagA|_|) (n : XNode) =
-        match n with
-        | Tag t -> 
-            let name = t.Name.LocalName
-            let attr = getAttr t
-            let children = t.Nodes() |> Seq.toList
-            Some(name,attr,children)
-        | _ -> None
-
-    let (|Elem|) (e: XElement) =
-        let name = e.Name.LocalName
-        let attr = getAttr e
-        name,attr,e.Value
