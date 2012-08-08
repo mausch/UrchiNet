229a9aff8a9634f7f816dd2dd9e871074bc58085
mausch <mauricioscheffer@gmail.com>
2012-08-08 18:42:35 -0300

    Bump version to 0.2

--- UrchiNet/AssemblyInfo.fs
+++ UrchiNet/AssemblyInfo.fs
@@ -4,8 +4,8 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyVersion("0.1.0.0")>]
-[<assembly: AssemblyFileVersion("0.1.0.0")>]
+[<assembly: AssemblyVersion("0.2.0.0")>]
+[<assembly: AssemblyFileVersion("0.2.0.0")>]
 [<assembly: AssemblyTitle("UrchiNet")>]
 [<assembly: AssemblyDescription("Urchin API v1 client")>]
 [<assembly: AssemblyProduct("UrchiNet")>]
