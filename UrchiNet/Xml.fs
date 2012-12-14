cc2f30bd28c2a94729b89f284a7eb7a1f8de6476
mausch <mauricioscheffer@gmail.com>
2012-12-14 17:13:20 -0300

    docs

--- UrchiNet/Xml.fs
+++ UrchiNet/Xml.fs
@@ -4,16 +4,33 @@ module Xml =
     open UrchiNet.Helpers
     open System.Xml.Linq
 
+    /// <summary>
+    /// Try to get an element in a XML container
+    /// </summary>
+    /// <param name="n">Element name</param>
+    /// <param name="e">Container</param>
     let tryElement (n: string) (e: XContainer) = 
         match e.Element(XName.Get n) with
         | null -> None
         | x -> Some x
 
+    /// <summary>
+    /// Gets an element from a container by element name. Throws if not found.
+    /// </summary>
+    /// <param name="n">Element name</param>
+    /// <param name="e">Container</param>
     let element (n: string) (e: XContainer) = 
         match tryElement n e with
         | None -> failwithf "Element %s not found" n
         | Some x -> x
 
+    /// <summary>
+    /// Gets an element from a container by namespace and element name.
+    /// Throws if not found.
+    /// </summary>
+    /// <param name="ns">Namespace</param>
+    /// <param name="n">Element name</param>
+    /// <param name="e">Container</param>
     let elementNS (ns: string) (n: string) (e: XContainer) = 
         let ns = XNamespace.Get ns
         let nname = ns + n
