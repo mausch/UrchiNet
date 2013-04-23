6eba457fc073e37426065ca2306fe86e3d449d08
mausch <mauricioscheffer@gmail.com>
2013-04-23 20:33:44 -0300

    Merge AsyncService to Service

--- UrchiNet/CSharp.fs
+++ UrchiNet/CSharp.fs
@@ -2,8 +2,9 @@
 
 open FSharpx
 
-/// API methods with async execution
-type AsyncService(config: Config) =
+/// API methods
+type Service(config: Config) =
+
     /// Retrieves the list of accounts for an authenticated user. 
     member x.AsyncGetAccountList() = getAccountList config
 
@@ -16,30 +17,26 @@ type AsyncService(config: Config) =
     /// Retrieves data for the specified dimensions/metrics
     member x.AsyncGetData query = getData config query
 
-/// API methods
-type Service(config: Config) =
-    let s = AsyncService(config)
+    /// Retrieves the list of accounts for an authenticated user. 
+    member x.GetAccountList() = x.AsyncGetAccountList() |> Async.RunSynchronously
 
     /// Retrieves the list of accounts for an authenticated user. 
-    member x.GetAccountList() = s.AsyncGetAccountList() |> Async.RunSynchronously
-
-    /// Retrieves the list of accounts for an authenticated user. 
-    member x.GetAccountListOrThrow() = s.AsyncGetAccountList() |> Async.RunSynchronously |> Choice.get
+    member x.GetAccountListOrThrow() = x.AsyncGetAccountList() |> Async.RunSynchronously |> Choice.get
 
     /// Retrieves a list of profiles for an authenticated user and account.
-    member x.GetProfileList accountId = s.AsyncGetProfileList accountId |> Async.RunSynchronously
+    member x.GetProfileList accountId = x.AsyncGetProfileList accountId |> Async.RunSynchronously
 
     /// Retrieves a list of profiles for an authenticated user and account.
-    member x.GetProfileListOrThrow accountId = s.AsyncGetProfileList accountId |> Async.RunSynchronously |> Choice.get
+    member x.GetProfileListOrThrow accountId = x.AsyncGetProfileList accountId |> Async.RunSynchronously |> Choice.get
 
     /// Retrieves list of tables for a specified profile. 
-    member x.GetTableList profileId = s.AsyncGetTableList profileId |> Async.RunSynchronously
+    member x.GetTableList profileId = x.AsyncGetTableList profileId |> Async.RunSynchronously
 
     /// Retrieves list of tables for a specified profile. 
-    member x.GetTableListOrThrow profileId = s.AsyncGetTableList profileId |> Async.RunSynchronously |> Choice.get
+    member x.GetTableListOrThrow profileId = x.AsyncGetTableList profileId |> Async.RunSynchronously |> Choice.get
 
     /// Retrieves data for the specified dimensions/metrics
-    member x.GetData query = s.AsyncGetData query |> Async.RunSynchronously
+    member x.GetData query = x.AsyncGetData query |> Async.RunSynchronously
 
     /// Retrieves data for the specified dimensions/metrics
-    member x.GetDataOrThrow query = s.AsyncGetData query |> Async.RunSynchronously |> Choice.get
+    member x.GetDataOrThrow query = x.AsyncGetData query |> Async.RunSynchronously |> Choice.get
